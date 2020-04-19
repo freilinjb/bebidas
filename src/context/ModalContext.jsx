17e5478e91aa3be72a269a8a9a0905c22c5530ca
freilinjb <freilinjb@gmail.com>
2020-04-19 19:48:37 -0400

    consultando API para el idDrink y en ModalContext

--- src/context/ModalContext.jsx
+++ src/context/ModalContext.jsx
@@ -9,6 +9,21 @@ const ModalProvider = (props) => {
     //state del provider
     const [idreceta, guardarIdReceta ] = useState(null);
 
+    //una vez que tenemos una receta, llamar a la API
+    useEffect(() => {
+        const obtenerReceta = async () => {
+            if(!idreceta) return;
+
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idreceta}`;
+
+            const resultado = await Axios.get(url);
+            console.log(resultado.data.drinks[0]);
+            
+             
+        }
+        obtenerReceta();
+    },[idreceta]);
+
     return ( 
         <ModalContext.Provider
             value={{
