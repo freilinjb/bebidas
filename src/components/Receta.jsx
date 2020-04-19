f5594133967703c11eaa4e7d21dfedf2b6134138
freilinjb <freilinjb@gmail.com>
2020-04-19 19:00:38 -0400

    disenando card Receta

--- src/components/Receta.jsx
+++ src/components/Receta.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 
 const Receta = ({receta}) => {
+    
+    const { strDrinkThumb, strDrink} = receta;
+
     return ( 
-        <p>Receta</p>
+        <div className="col-md-4 mb-3">
+            <div className="card">
+                <h2 className="card-header">{receta.strDrink}</h2>
+
+                <img src={strDrinkThumb} alt={`Imagen de ${strDrink}`} className="card-imt-top"/>
+
+                <div className="card-body">
+                    <button className="btn btn-block btn-danger" type="button">Ver Bebida </button>
+                </div>
+            </div>
+        </div>
      );
 }
  
